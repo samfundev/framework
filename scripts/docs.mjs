bb9764b824d1cabec8dcde7ec905ba6c6d8d5077
Jeroen Claassens <support@favware.tech>
2021-12-30 22:03:22 +0100

    chore: fixes for doc parser

--- scripts/docs.mjs
+++ scripts/docs.mjs
@@ -3,5 +3,7 @@ import { runGenerator } from '@discordjs/ts-docgen';
 runGenerator({
 	existingOutput: 'docs/typedoc-out.json',
 	custom: 'docs/index.yml',
-	output: 'docs/docs.json'
+	output: 'docs/framework.json',
+	verbose: true,
+	spaces: 4
 });
